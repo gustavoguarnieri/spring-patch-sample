0d6fde81f0f867460836af9b285b9e0d5dcf28e4
Gustavo <gustavo.guarnieri@gmail.com>
2022-07-22 00:07:45 -0300

    added application class

---
+++ src/main/kotlin/br/com/example/spring/patch/sample/Application.kt
@@ -0,0 +1,22 @@
+package br.com.example.spring.patch.sample
+
+import br.com.example.spring.patch.sample.constants.Products
+import br.com.example.spring.patch.sample.repositories.ProductRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCachingKotlinSampleApplication(
+    private val repository: ProductRepository
+) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        repository.deleteAll()
+        Products.productCatalog.forEach(repository::save)
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<SpringCachingKotlinSampleApplication>(*args)
+}
